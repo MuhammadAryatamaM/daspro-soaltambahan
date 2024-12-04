89d2a808554d472d61043803364598d85d856941
MuhammadAryatamaM <muhammad.aryatama2006@gmail.com>
2024-12-04 10:18:30 +0700

    program rerata 3 mahasiswa masing-masing 5 nilai

--- Array 2D/RerataNilai.java
+++ Array 2D/RerataNilai.java
@@ -3,12 +3,12 @@ public class RerataNilai {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        double avg = 0;
         int[][] nilai = new int[3][5];
         double[] rerata = new double[3];
-
+        
         System.out.println("===== INPUT NILAI =====");
         for (int i = 0; i < 3; i++) {
+            double avg = 0;
             for (int j = 0; j < 5; j++) {
                 System.out.printf("Mahasiswa ke-%d, nilai ke-%d: ", (i+1), (j+1));
                 nilai[i][j] = sc.nextInt();
